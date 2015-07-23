7024815de5598f5eb4fe7a0c02bde3010c3065b9
yukipyon <wolfvbear@gmail.com>
2015-07-23 19:11:33 +0000

    Extended the demo to parse items.

--- xml-demo2.rkt
+++ xml-demo2.rkt
@@ -2,8 +2,26 @@
 (require xml xml/path)
 (require racket/port)
 
-(define input (open-input-file "input"))
-(define data 
-    (read-xml input))
+(define input (port->string (open-input-file "input")))
+(define input-wrapped
+  (let ([t
+          (regexp-replace* "<item>" input "<item-wrapper><item>")])
+    (regexp-replace* #rx"</item>" t "</item></item-wrapper>")))
 
-(write-xexpr (xml->xexpr (document-element data)))
+(define input-x (string->xexpr input-wrapped))
+
+(define items (se-path*/list '(item-wrapper) input-x))
+
+(map (λ (n)
+        (define title (se-path* '(title) n))
+        (define price (se-path* '(currentPrice) n))
+        (define loc (se-path* '(location) n))
+        (define con (se-path* '(conditionDisplayName) n))
+        (displayln "########################################")
+;        (displayln n)
+        (when (and title price)
+          (printf "Title: ~a~nPrice: ~a~nLocation: ~a~nCondition: ~a~n"
+                  title price loc con))
+        (displayln "########################################")
+        ) items)
+
