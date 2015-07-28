38675a62ae444b74aabe8d5ede0a7d13f6956877
yukipyon <wolfvbear@gmail.com>
2015-07-28 19:17:21 +0000

    URL demo using Polipo and Tor.

--- url-demo2.rkt
+++ url-demo2.rkt
@@ -4,7 +4,7 @@
 (define url-s "http://www.bing.com/")
 (define url-u (string->url url-s))
 
-(current-proxy-servers (list (list "http" "192.168.100.1" 4444)))
+(current-proxy-servers (list (list "http" "127.0.0.1" 8123)))
 (define in (get-impure-port url-u))
 (define content (port->string in))
 
