458814a7dbf7659382f3f0024377f57f0b48a1b0
yukipyon <wolfvbear@gmail.com>
2015-08-01 19:01:45 +0000

    Added base url for Ebay API.

--- build-query.rkt
+++ build-query.rkt
@@ -1,3 +1,14 @@
 #lang racket
 
-(displayln (getenv "EBAY_API_KEY"))
+(define API-KEY (getenv "EBAY_API_KEY"))
+
+(define base-url 
+  (string-append 
+    "http://svcs.ebay.com/services/search/FindingService/v1?"
+    "OPERATION-NAME=findItemsByKeywords"
+    "&SERVICE-VERSION=1.0.0"
+    "&SECURITY-APPNAME=" API-KEY
+    "&RESPONSE-DATA-FORMAT=XML"
+    "&REST-PAYLOAD"))
+
+(displayln base-url)
