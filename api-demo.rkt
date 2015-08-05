f5db527866a78e406778b1203b209ac6b3378068
yukipyon <wolfvbear@gmail.com>
2015-08-05 19:01:16 +0000

    Added URL fetching and XML parsing.

--- api-demo.rkt
+++ api-demo.rkt
@@ -4,4 +4,14 @@
 (require "parse-xml.rkt")
 
 (define q (add-filter-gbp (add-keyword base-url "harry potter")))
-(displayln (get-url q))
+(define xml (get-url q))
+(define items (xml->items xml))
+
+(for ([n items])
+        (displayln "########################################")
+        (displayln (item-title n))
+        (displayln (item-price n))
+        (displayln (item-location n))
+        (displayln (item-condition n))
+        (displayln (item-url n))
+        (displayln "########################################"))
