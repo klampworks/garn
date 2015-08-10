f1320e4e535caef7483ec0d1a9789d03384add7c
yukipyon <wolfvbear@gmail.com>
2015-08-10 19:15:10 +0000

    Added proper main file.

---
+++ garn.rkt
@@ -0,0 +1,28 @@
+#lang racket
+(require "build-query.rkt")
+(require "get-url.rkt")
+(require "parse-xml.rkt")
+
+(define keywords (vector-ref (current-command-line-arguments) 0))
+
+(define q
+  (add-filter-used
+      (add-keyword base-url keywords)))
+
+(define xml (get-url q))
+
+(define items
+  (convert-currency
+    (ship-to-uk-only
+      (xml->items xml))))
+
+(for ([n  items])
+        (displayln "########################################")
+        (displayln (item-title n))
+        (displayln (item-price n))
+        (displayln (item-location n))
+        (displayln (item-condition n))
+        (displayln (item-url n))
+        (displayln (item-currency n))
+        (displayln "########################################"))
+
