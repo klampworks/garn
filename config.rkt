b855ced85a4e989e56a7e58435129523842dc562
yukipyon <wolfvbear@gmail.com>
2015-08-12 19:17:17 +0000

    Moved proxy server configuration into config.rkt.

--- config.rkt
+++ config.rkt
@@ -1,6 +1,7 @@
 #lang racket
 (require "parse-xml.rkt")
 (provide apply-filters)
+(require net/url)
 
 (define filters 
   (list 
@@ -9,3 +10,7 @@
 
 (define (apply-filters items)
   ((apply compose filters) items))
+
+;socksParentProxy = localhost:9050
+;;dnsUseGethostbyname = no
+(current-proxy-servers (list (list "http" "127.0.0.1" 8123)))
--- get-url.rkt
+++ get-url.rkt
@@ -2,9 +2,5 @@
 (provide get-url)
 (require net/url)
 
-;socksParentProxy = localhost:9050
-;;dnsUseGethostbyname = no
-(current-proxy-servers (list (list "http" "127.0.0.1" 8123)))
-
 (define (get-url url-s)
   (call/input-url (string->url url-s) get-pure-port port->string))
